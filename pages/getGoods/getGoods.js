0652e464f80f801f211b7f101f19ffb9c542ba51
蒙栎 <mengli@qingmutec.com>
2019-06-20 18:41:48 +0800

    do something

--- pages/getGoods/getGoods.js
+++ pages/getGoods/getGoods.js
@@ -5,7 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showPic: false
+    showPic: false,
+    showPicker: false,
+    value: '',
+    columns: ['全部','金富丽','女人街','大西豪','大时代','国投','国大','国润'],
+    result: '',
+    fakeData: [{
+      'Id': '1321',
+      'GetGoodsNum': 'A12345678',
+      'Price': '30',
+      'Amount': 1,
+      'Remake': '',
+      'Img': 'https://xkerp-pic.oss-cn-shenzhen.aliyuncs.com/A1038.jpg?x-oss-process=image/resize,h_300,limit_0'
+    },{
+      'Id': '1322',
+      'GetGoodsNum': 'A12345678',
+      'Price': '58',
+      'Amount': 2,
+      'Remake': '条纹的',
+      'Img': 'https://xkerp-pic.oss-cn-shenzhen.aliyuncs.com/A1037.jpg?x-oss-process=image/resize,h_300,limit_0'
+    },{
+      'Id': '1323',
+      'GetGoodsNum': 'A12345678',
+      'Price': '26',
+      'Amount': 5,
+      'Remake': '七分袖',
+      'Img': 'https://xkerp-pic.oss-cn-shenzhen.aliyuncs.com/A1036.jpg?x-oss-process=image/resize,h_300,limit_0'
+    }],
+    selectList: {}
   },
 
   /**
@@ -65,18 +92,48 @@ Page({
   },
 
   /**
-   * 点击商品卡片事件
+   * 点击商品图片事件
    */
-  handlePic: function (event) {
+  onClickThumb() {
+    console.log("111")
     this.setData({ showPic: true });
-    console.log(event);
   },
 
   /**
-   * 关闭图片
+   * 点击选择商品卡片
+   */
+  onClickCard(e) {
+    console.log(e)
+    const id = e.currentTarget.dataset.item.Id
+    const selectListItem = "selectList." + id
+    if (this.data.selectList[id] === undefined | this.data.selectList[id] === false) {
+      this.setData({
+        [selectListItem]: true
+      })
+    } else {
+      this.setData({
+        [selectListItem]: false
+      })
+    }
+  },
+
+  /**
+   * 关闭popup
    */
   onClose() {
+    console.log("this")
     this.setData({ showPic: false });
-  }
+    this.setData({ showPicker: false })
+  },
 
+  pickerTest() {
+    this.setData({ showPicker: true })
+  },
+
+  confirmPicker(event) {
+    const { picker, value, index } = event.detail
+    console.log(event)
+    console.log(value)
+    this.setData({ showPicker: false})
+  }
 })
